b8a162a24ffdde261cb483255512b72c31c21536
Aleksey <lekseysz@yandex.ru>
2024-10-17 18:14:55 +0300

    refactor: add player class for different strategies and more test coverage

---
+++ lib/player/stay_player.rb
@@ -0,0 +1,5 @@
+class StayPlayer < BasePlayer
+  def second_guess(other_door)
+    @guess
+  end
+end
---
+++ lib/player/switch_player.rb
@@ -0,0 +1,5 @@
+class SwitchPlayer < BasePlayer
+  def second_guess(other_door)
+    other_door
+  end
+end
